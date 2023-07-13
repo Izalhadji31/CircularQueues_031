7a9757e22f3ef193df0da07a3175e6f26657a86d
Izalhadji31 <ryzalhadji@gmail.com>
2023-07-13 12:09:51 +0700

    Jika FRONT_position <= REAR_position, iterasi FRONT hinnga REAR

--- CircularQueues_031.cpp
+++ CircularQueues_031.cpp
@@ -71,3 +71,13 @@ public:
 		}
 
 		cout << "\nElements in the Queue are : \n";
+
+		//Jika FRONT_position <= REAR_position, iterasi FRONT hinnga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position)
+			{
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
